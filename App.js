86d91b6b3cea385741f5bffa9b3af0daf0979bbd
QDLu <quangdunglu159@gmail.com>
2023-01-09 07:19:54 +0700

    Function config webrtc

--- App.js
+++ App.js
@@ -16,6 +16,11 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {
+  mediaDevices,
+  MediaStream,
+  RTCPeerConnection,
+} from 'react-native-webrtc';
 
 import {
   Colors,
@@ -24,6 +29,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {useRef, useState} from 'react/cjs/react.development';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -59,6 +65,97 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  // setup webrtc
+  const [remoteStream, setRemoteStream] = useState(null);
+  const [localStream, setLocalStream] = useState(null);
+  const [webcamStarted, setWebcamStarted] = useState(false);
+  const [channelId, setChannelId] = useState(null);
+  const pc = useRef();
+  const servers = {
+    iceServers: [
+      {
+        urls: [
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+        ],
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  };
+
+  const startWebcam = async () => {
+    pc.current = new RTCPeerConnection(servers);
+    const local = await mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    pc.current.addStream(local);
+    setLocalStream(local);
+
+    const remote = new MediaStream();
+    setRemoteStream(remote);
+
+    // Push tracks from local stream to peer connection
+    local.getTracks().forEach(track => {
+      pc.current.getLocalStreams()[0].addTrack(track);
+    });
+
+    // Pull tracks from peer connection, add to remote video stream
+    pc.current.ontrack = event => {
+      event.streams[0].getTracks().forEach(track => {
+        remote.addTrack(track);
+      });
+    };
+
+    pc.current.onaddstream = event => {
+      setRemoteStream(event.stream);
+    };
+  };
+
+  // const startCall = async () => {
+  //   const channelDoc = firestore().collection('channels').doc();
+  //   const offerCandidates = channelDoc.collection('offerCandidates');
+  //   const answerCandidates = channelDoc.collection('answerCandidates');
+
+  //   setChannelId(channelDoc.id);
+
+  //   pc.current.onicecandidate = async event => {
+  //     if (event.candidate) {
+  //       await offerCandidates.add(event.candidate.toJSON());
+  //     }
+  //   };
+
+  //   //create offer
+  //   const offerDescription = await pc.current.createOffer();
+  //   await pc.current.setLocalDescription(offerDescription);
+
+  //   const offer = {
+  //     sdp: offerDescription.sdp,
+  //     type: offerDescription.type,
+  //   };
+
+  //   await channelDoc.set({offer});
+
+  //   // Listen for remote answer
+  //   channelDoc.onSnapshot(snapshot => {
+  //     const data = snapshot.data();
+  //     if (!pc.current.currentRemoteDescription && data?.answer) {
+  //       const answerDescription = new RTCSessionDescription(data.answer);
+  //       pc.current.setRemoteDescription(answerDescription);
+  //     }
+  //   });
+
+  //   // When answered, add candidate to peer connection
+  //   answerCandidates.onSnapshot(snapshot => {
+  //     snapshot.docChanges().forEach(change => {
+  //       if (change.type === 'added') {
+  //         const data = change.doc.data();
+  //         pc.current.addIceCandidate(new RTCIceCandidate(data));
+  //       }
+  //     });
+  //   });
+  // };
 
   return (
     <SafeAreaView style={backgroundStyle}>
